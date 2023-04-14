ba947090458681282bb6615b4c0fa0effc0aae39
Prabhas3699 <prabhak620@gmail.com>
2023-04-14 08:41:13 +0530

    Keyword Driven Framework for Amazon

--- src/test/java/com/bridgelabz/keyword/HomePageTest.java
+++ src/test/java/com/bridgelabz/keyword/HomePageTest.java
@@ -3,12 +3,12 @@ package com.bridgelabz.keyword;
 import com.bridgelabz.k.engine.KeywordEngine;
 import org.testng.annotations.Test;
 
-public class LoginTest {
+public class HomePageTest {
     KeywordEngine keywordEngine;
-
     @Test
-    public void loginPageTest(){
+    public void homePageTest(){
         keywordEngine=new KeywordEngine();
         keywordEngine.starExecution("login");
+        keywordEngine.starExecution("home");
     }
 }
